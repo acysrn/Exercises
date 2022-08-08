8d2930a025924c65eaa48329714c29d2f6c54146
acysrn <101911435+acysrn@users.noreply.github.com>
2022-08-07 22:27:21 -0500

    Add files via upload

---
+++ How the Web Works Exercise.js
@@ -0,0 +1,22 @@
+// In your own terms, define the following terms:
+
+//What is HTTP?
+//Hyper text transfer protocol
+//What is a URL?
+// it is an address for a website
+//What is DNS?
+// Domain Name System. It converts URLs to IP addresses
+//What is a query string?
+// This allows key-value pairs to be passed into the URL
+//What are two HTTP verbs and how are they different?
+// GET - gets data from server
+// POST - sends data to server
+//What is an HTTP request?
+// a request from the client to a server which follows the HTTP protocol
+//What is an HTTP response?
+// a response from a server to a client
+//What is an HTTP header? Give a couple examples of request and response headers you have seen.
+// They provide additional information about the request or response
+// examples: request headers - host, accept, cookie; response headers - set-cookie, cache-control
+//What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser?
+// the browser makes a request to that IP address, the server sends a response, the browser makes a DOM from the HTML, the browser makes separate HTTP requests and receives responses form the server
